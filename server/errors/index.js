3cb317745fc6281fa790b45cecd3076901a3ed25
ankit patidar <patiankit6555@gmail.com>
2023-09-28 08:40:38 +0530

    yes

--- server/errors/index.js
+++
@@ -1,6 +0,0 @@
-import BadRequestError from './bad-request.js'
-import NotFoundError from './not-found.js'
-import CustomApiError from './custom-api.js'
-import UnAuntheticatedError from './unauthenticate.js'
-
-export { BadRequestError, NotFoundError, CustomApiError, UnAuntheticatedError }
--- server/middleware/notfound.js
+++
@@ -1,4 +0,0 @@
-const notFound = (req, res) => {
-  res.status(404).json({ msg: 'this route does not exist' })
-}
-export default notFound
